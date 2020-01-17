f2030e9208347def3e574fe99c6d70867816cffd
tatitakan <tatitakan43@gmail.com>
2020-01-17 10:36:32 +0700

    LAB2

---
+++ LAB2/610112418010/www/php/test.php
@@ -0,0 +1,40 @@
+<<!DOCTYPE html>
+<html>
+<body>
+<?php
+echo "Hello world !!!";
+
+$sname = "Archtaweekul";
+$sports = array("Swimming","running");
+
+?>
+<hr>
+<?="My name is Tatitakan " . $sname ?>
+
+<form action="../action/post.php" method="POST">
+    <select name="sl_sport">
+        <option>- Select -</option>
+        <option value="1">Football</option>
+        <option value="2">Tenis</option>
+    <?php
+        foreach($sports as $sport){
+    ?>
+            <option value="<?=$key+3?>"><?=$sport?></option>
+    <?php } ?>
+    </select>
+<button type="submit"> SEND </button>
+</form>
+
+<lable id="lb_post"></lable>
+
+</body>
+</html>
+<script src="../js/jquery-3.4.1.min.js"></script>
+<script>
+$(function(){
+    $("select").change(function(){
+        sp_val = $(this).val();
+        $("#lb_post").load("../action/post.php?getSport=" + sp_val);
+    });
+});
+</script>
